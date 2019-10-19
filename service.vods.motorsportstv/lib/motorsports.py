90207d4ec3415d0559194d38aa1ffe72323354f7
boogie <boogie@gmx.com>
2019-10-20 01:30:37 +0200

    motorsports: refactoring

--- service.vods.motorsportstv/lib/motorsports.py
+++ service.vods.motorsportstv/lib/motorsports.py
@@ -18,10 +18,13 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 import re
-import urllib
+import datetime
 import vods
 import htmlement
 import json
+
+from tinyxbmc.net import kodiurl
+from hamcrest.library.collection import isin
 
 
 class motorsports(vods.showextension):
@@ -62,26 +65,82 @@ class motorsports(vods.showextension):
         page = self.download(uri)
         return json.loads(re.search("window\.APP_STATE\=(.+?)<\/script>", page).group(1))
 
+    def epiimg(self, episode):
+        if "images" in episode and "retina" in episode["images"]:
+            return episode["images"]["retina"]
+        elif "contentImages" in episode and "retina" in episode["contentImages"]:
+            return episode["contentImages"]["retina"]
+        elif "coverImages" in episode and "retina" in episode["coverImages"]:
+            return episode["coverImages"]["retina"]
+        else:
+            return""
+
+    def epilink(self, episode):
+        if "link" in episode:
+            return True, episode["link"]
+        elif "videoFile" in episode:
+            links = []
+            suburl = episode["videoFile"]["path"]
+            if suburl.startswith("http://") or suburl.startswith("https://"):
+                links.append(kodiurl(suburl, headers={"Referer": self.domain}))
+            else:
+                url = "%s/%s/playlist.m3u8" % (episode["videoBaseUrl"],
+                                               suburl)
+                links.append(kodiurl(url, headers={"Referer": self.domain}))
+                url = "%s/%s/playlist.m3u8" % (episode["videoBaseUrlUnsecure"],
+                                               suburl)
+                links.append(kodiurl(url, headers={"Referer": self.domain}))
+            return False, links
+
+    def epidate(self, episode):
+        key = "date"
+        if episode.get("episodeDate"):
+            key = "episodeDate"
+        dt = episode[key]
+        if isinstance(dt, (int, float)):
+            return datetime.datetime.fromtimestamp(float(dt)/1000)
+        else:
+            return datetime.datetime.strptime(episode[key][:-6], "%Y-%m-%dT%H:%M:%S")
+
     def getepisodes(self, show=None, sea=None):
         uri, art, cat = show
         js = self.getjson(self.domain + uri)
-        for carousel in js[cat + "Item"]["response"]["carousels"]:
-            for episode in carousel["episodes"]:
-                if not episode["type"] == "default":
-                    continue
-                epiart = art.copy()
-                epiart["icon"] = epiart["icon"] = epiart["icon"] = episode["images"]["retina"]
-                print episode["date"]
-                self.additem(carousel["title"] + " : " + episode[u"title"], episode[u"link"], None, epiart)
+
+        if cat == "racing":
+            episodes = [x[1] for x in js["racingItem"]["response"]["entities"]["episode"].iteritems()]
+        else:
+            episodes = []
+            for carousel in js[cat + "Item"]["response"]["carousels"]:
+                episodes.extend(carousel["episodes"])
+
+        for episode in episodes:
+            episode["date"] = self.epidate(episode)
+
+        for episode in sorted(episodes, key=lambda i: i["date"], reverse=True):
+            titles = []
+            if episode.get("livestream"):
+                titles.append("[LIVE]")
+            if not episode["type"] == "default" and False:
+                return
+            epiart = art.copy()
+            epiart["icon"] = epiart["icon"] = epiart["icon"] = self.epiimg(episode)
+            try:
+                titles.append(episode["date"].strftime("%d.%m.%Y"))
+            except Exception:
+                pass
+            titles.append(episode[u"title"])
+            self.additem(" - ".join(titles), self.epilink(episode), None, epiart)
 
     def geturls(self, url):
-        url = self.domain + url
-        print url
-        tree = htmlement.fromstring(self.download(url))
-        video = tree.find(".//video")
-        print video
-        if video is not None:
-            headers = {"Referer": url}
-            kodiurl = "%s|%s" % (video.get("src"), urllib.urlencode(headers))
-            print kodiurl
-            yield kodiurl
+        resolve, url = url
+        if not resolve:
+            for u in url:
+                yield u
+        if resolve:
+            url = self.domain + url
+            print url
+            tree = htmlement.fromstring(self.download(url))
+            video = tree.find(".//video/source")
+            print video
+            if video is not None:
+                yield kodiurl(video.get("src"), headers={"Referer": url})
