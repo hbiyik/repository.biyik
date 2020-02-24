a871fc7f4b3727322c043a769cec3dd737b7d625
boogie <boogie@boogie-lin>
2020-02-24 01:30:57 +0100

    cachecontrol: use new hay interface

--- script.module.cachecontrol/lib/cachecontrol/caches/hay_cache.py
+++ script.module.cachecontrol/lib/cachecontrol/caches/hay_cache.py
@@ -4,7 +4,7 @@ from tinyxbmc import hay
 
 class HayCache(BaseCache):
     def __init__(self, directory, maxrecords=5000):
-        self.hay = hay.stack(directory, "null", 0, maxrecords, True, True)
+        self.hay = hay.stack(directory, "null", 0, maxrecords)
 
     def get(self, key):
         data = self.hay.find(key).data
