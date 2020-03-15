c6a99eeb8f3fe6675e85d2a7a2a2910b2a6371c2
boogie <boogie@boogie-lin>
2020-03-15 20:43:23 +0100

    ghub: cleaning

--- script.module.ghub/lib/ghub.py
+++ script.module.ghub/lib/ghub.py
@@ -90,10 +90,53 @@ def _updatezip(u, tdir, rname):
     zp.extractall(tdir)
 
 
-def load(uname, rname, branch, commit=None, path=[], period=24):
+def _load(uname, rname, branch, commit, path, period):
+    mem = None
+    bdir = _mkdir(_ddir, uname)
+
+    # load memory file
+    if branch:
+        bdir = _mkdir(bdir, "branch", branch)
+    else:
+        bdir = _mkdir(bdir, "release")
+    memfile = os.path.join(bdir, rname + ".json")
+    if os.path.exists(memfile):
+        with open(memfile) as infile:
+            mem = json.load(infile)
+
+    # check if we need to update
+    if mem and time.time() - mem["ts"] < period * 60 * 60:
+        print "GITHUB: INFO: Using Existing, no need to check: %s Repo:%s Branch:%s Commit: %s" % (uname, rname, branch, commit)
+        return bdir
+    else:
+        # get latest commits
+        try:
+            if branch:
+                ref = _getcommits(uname, rname, branch, commit)
+            else:
+                ref = _getrels(uname, rname)
+        except Exception:
+            print "GITHUB: WARNING: Can not get latest meta: User:%s Repo:%s Branch:%s Commit: %s" % (uname, rname, branch, commit)
+            return
+
+        # download new package, extract and update the memory file
+        lcommit, zipu = ref[0]
+        if not mem or not lcommit == mem["latest"]:
+            print "GITHUB: INFO: Updating to commit %s->%s for %s Repo:%s Branch:%s Commit: %s" % (lcommit, zipu,
+                                                                                                   uname, rname, branch, commit)
+            _updatezip(zipu, bdir, rname)
+            data = {"ts": time.time(),
+                    "latest": lcommit
+                    }
+            with open(memfile, 'w') as outfile:
+                json.dump(data, outfile)
+        return bdir
+
+
+def load(uname, rname, branch, commit=None, path=None, period=24):
     """
     Loads, caches, and arranges paths for a repo from github.
-    This module, downloads a github repo to 
+    This module, downloads a github repo to
     userdata/addon_data/scirpt.module.gub/uname/package/branch/commit
     It also updates the package automatically in a given period (24h default)
     Note that this is not a package manager and does not do any dependency check
@@ -108,67 +151,30 @@ def load(uname, rname, branch, commit=None, path=[], period=24):
 
     Params:
         uname: github username
-        rname: repository name 
+        rname: repository name
         branch: github branch (ie master), if this parameter is None, latest tagged
             version is used.
         commit: [optional,None] commit of the specified branch, if None is specified
             latest commit is fetched, if brach is None, this parameter is dismissed
         path: [optional,[]] list of directories pointing to the root directory of source
             ie, if source is in lib/src folder path should be ["lib", "src"]
-        period: [optional, 24] period in hours of how frequent the repo should be checked 
+        period: [optional, 24] period in hours of how frequent the repo should be checked
             for an update
 
     Returns:
         None
 
     """
-    mem = None
-    bdir = _mkdir(_ddir, uname)
-    
-    # load memory file
-    if branch:
-        bdir = _mkdir(bdir, "branch", branch)
-    else:
-        bdir = _mkdir(bdir, "release")
-    memfile = os.path.join(bdir, rname + ".json")
-    if os.path.exists(memfile):
-        with open(memfile) as infile:
-            mem = json.load(infile)
-    
-    # prepare path
-    sdir = None
-    for d in os.listdir(bdir):
-        sdir = os.path.join(bdir, d)
-        if os.path.isdir(sdir) and d.startswith(rname):
-            break
-    if sdir and sdir not in sys.path:
-        sys.path.append(os.path.join(sdir, *path))
-    
-    # check if we need to update
-    if mem and time.time() - mem["ts"] < period * 60 * 60:
-        print "GITHUB: INFO: Using Existing, no need to check: %s Repo:%s Branch:%s Commit: %s" % (uname, rname, branch, commit)
-        return True
-    else:
-        # get latest commits
-        try:
-            if branch:
-                ref = _getcommits(uname, rname, branch, commit)
-            else:
-                ref = _getrels(uname, rname)
-        except Exception:
-            print "GITHUB: WARNING: Can not get latest meta: User:%s Repo:%s Branch:%s Commit: %s" % (uname, rname, branch, commit)
-            return False
-        
-        # download new package, extract and update the memory file
-        lcommit, zipu = ref[0]
-        if not mem or not lcommit == mem["latest"]:
-            print "GITHUB: INFO: Updating to commit %s->%s for %s Repo:%s Branch:%s Commit: %s" % (lcommit, zipu,
-                                                                                                   uname, rname, branch, commit)
-            _updatezip(zipu, bdir, rname)
-            data = {
-                    "ts": time.time(),
-                    "latest": lcommit
-                    }
-            with open(memfile, 'w') as outfile:
-                json.dump(data, outfile)
-        return True
+    if not path:
+        path = []
+    bdir = _load(uname, rname, branch, commit, path, period)
+    # prepare the path
+    if bdir:
+        sdir = None
+        for d in os.listdir(bdir):
+            sdir = os.path.join(bdir, d)
+            if os.path.isdir(sdir) and d.startswith(rname):
+                break
+        if sdir and sdir not in sys.path:
+            sys.path.append(os.path.join(sdir, *path))
+        return sdir
