7e8daf7c5b23bbeb71b2388dbe95b1bdabb8a4c5
hbiyik <huseyinbiyik@hotmail.com>
2020-11-07 22:25:53 +0100

    tinyxbmc: update debugger location

--- script.module.tinyxbmc/lib/tinyxbmc/container.py
+++ script.module.tinyxbmc/lib/tinyxbmc/container.py
@@ -42,7 +42,7 @@ REMOTE_DBG = False
 if REMOTE_DBG:
     #pdevpath = "C:\\Users\\z0042jww\\.p2\\pool\\plugins\\org.python.pydev.core_7.2.1.201904261721\\pysrc"
     #pdevpath = "/home/boogie/.p2/pool/plugins/org.python.pydev.core_7.2.1.201904261721/pysrc/"
-    pdevpath = "/home/boogie/local/eclipse/plugins/org.python.pydev.core_7.6.0.202006041357/pysrc"
+    pdevpath = "/home/boogie/local/eclipse/plugins/org.python.pydev.core_8.0.0.202009061309/pysrc"
     sys.path.append(pdevpath)
     import pydevd  # @UnresolvedImport
     pydevd.settrace(stdoutToServer=True, stderrToServer=True, suspend=False)
