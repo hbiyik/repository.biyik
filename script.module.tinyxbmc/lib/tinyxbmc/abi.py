692ba8a01fb40dbae93cb037455eca96999b2ea9
hbiyik <huseyinbiyik@hotmail.com>
2021-11-07 00:22:08 +0100

    abi: machineid

--- script.module.tinyxbmc/lib/tinyxbmc/abi.py
+++ script.module.tinyxbmc/lib/tinyxbmc/abi.py
@@ -1,12 +1,20 @@
+# -*- coding: utf-8 -*-
 '''
 Created on Nov 1, 2021
 
 @author: boogie
 '''
+
 import platform
 import xbmc
 import struct
+import uuid
+import sys
+import random
+import binascii
 
+from tinyxbmc import const
+from tinyxbmc import tools
 
 elf_machines = {2: "sparc", 3: "x86", 8: "mips", 20: "ppc", 21: "ppc64", 22: "s390",
                 40: "arm", 42: "superh", 50: "ia64", 62: "amd64", 183: "aarch64", 243: "riscv"}
@@ -14,6 +22,12 @@ elf_machines = {2: "sparc", 3: "x86", 8: "mips", 20: "ppc", 21: "ppc64", 22: "s3
 
 def detect_os():
     system = platform.system().lower()
+    if xbmc.getCondVisibility('system.platform.osx'):
+        if xbmc.getCondVisibility('system.platform.atv2'):
+            return "osx-atv2"
+        return "osx"
+    if xbmc.getCondVisibility('system.platform.ios'):
+        return "ios"
     if "windows" in system or xbmc.getCondVisibility('system.platform.windows'):
         return "windows"
     if "linux" in system or xbmc.getCondVisibility('system.platform.linux'):
@@ -54,9 +68,44 @@ def getelfabi():
     elif machine in ["arm", "aarch64"]:
         if is64:
             toolchains.append("aarch64")
-        elif mflags_d["HARD"]:
+        elif mflags_d["HRD"]:
             toolchains.append("armhf")
             toolchains.append("armel")
         else:
             toolchains.append("armel")
     return toolchains, machine, is64, mflags
+
+
+def getmachineid():
+    mid = None
+    prefix = "m:"
+    getters = []
+    if hasattr(uuid, "_OS_GETTERS"):
+        getters = uuid._OS_GETTERS
+    elif hasattr(uuid, "_NODE_GETTERS_WIN32") and sys.platform == 'win32':
+        getters = uuid._NODE_GETTERS_WIN32
+    elif hasattr(uuid, "_NODE_GETTERS_UNIX"):
+        getters = uuid._NODE_GETTERS_UNIX
+
+    for getter in getters:
+        try:
+            mid = getter()
+        except Exception:
+            continue
+        if (mid is not None) and (0 <= mid < (1 << 48)):
+            break
+        else:
+            mid = None
+
+    if not mid:
+        prefix = "i:"
+        from tinyxbmc import hay
+        with hay.stack(const.OPTIONHAY) as stack:
+            mid = stack.find("installid").data
+            if not mid:
+                mid = random.getrandbits(48) | (1 << 40)
+                stack.throw("installid", mid)
+                stack.snapshot()
+
+    mid = binascii.hexlify(struct.pack("q", tools.hashfunc(struct.pack("Q", mid)[:-2]))).decode()
+    return prefix + mid
