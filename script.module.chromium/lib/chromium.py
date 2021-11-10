90d98ae04a6ecd511fd90ae26a8800a354144044
hbiyik <huseyinbiyik@hotmail.com>
2021-11-10 16:24:40 +0100

    chromium: validation support

--- script.module.chromium/lib/chromium.py
+++ script.module.chromium/lib/chromium.py
@@ -5,7 +5,9 @@ Created on Oct 31, 2021
 '''
 import websocket
 import json
+import traceback
 from six.moves.urllib.request import urlopen
+from six.moves.urllib.parse import quote
 
 
 class Browser:
@@ -37,6 +39,8 @@ class Browser:
         self.command("Network.enable")
         if self.useragent:
             self.command("Network.setUserAgentOverride", userAgent=self.useragent)
+        else:
+            self.useragent = json.loads(urlopen("%s/json/version" % self.url).read())["User-Agent"]
         self.command("DOM.enable")
 
     def connect(self):
@@ -44,9 +48,9 @@ class Browser:
             self.ws = websocket.create_connection(self.wsurl, enable_multithread=True)
 
     def command(self, method, **kwargs):
-        if self.debug:
-            print("Command: %s, %s" % (method, kwargs))
         self.id += 1
+        if self.debug:
+            print("Command (%s): %s, %s" % (self.id, method, kwargs))
         self.connect()
         self.ws.send(json.dumps({"id": self.id, "method": method, "params": kwargs}))
 
@@ -70,28 +74,46 @@ class Browser:
             msg_method = message.get("method")
             yield message, msg_id, msg_method
 
-    def navigate(self, url, referer=None):
+    def getcfheaders(self, url):
+        self.command("Network.getCookies", urls=[url])
+        for m, mid, _ in self.itermessages():
+            if mid == self.id:
+                cookies = m["result"]["cookies"]
+                break
+
+        return {"User-Agent": self.useragent,
+                "cookie": "; ".join(["%s=%s" % (c["name"], quote(c["value"])) for c in cookies])
+                }
+
+    def navigate(self, url, referer=None, validate=None):
         node = None
         html = None
         self.ws.settimeout(self.maxtimeout)
         kwargs = {"url": url}
         if referer:
-            kwargs["referer": referer]
+            kwargs["referer"] = referer
         self.command("Page.navigate", **kwargs)
         for message, msg_id, msg_method in self.itermessages():
             if msg_id:
                 nhtml = message.get("result", {}).get("outerHTML")
                 if nhtml and not nhtml == html:
-                    html = nhtml
-                    if not self.ws.gettimeout() == self.loadtimeout:
-                        self.ws.settimeout(self.loadtimeout)
-            if node and msg_method and msg_method.startswith("DOM."):
-                self.command("DOM.getOuterHTML", nodeId=node)
+                    if validate:
+                        try:
+                            isvalid = validate(nhtml)
+                            if isvalid:
+                                return isvalid
+                        except Exception:
+                            print(traceback.format_exc())
+                    else:
+                        html = nhtml
+                        if not self.ws.gettimeout() == self.loadtimeout:
+                            self.ws.settimeout(self.loadtimeout)
             if msg_id in self.nodecmds:
                 self.nodecmds.remove(msg_id)
                 nodeid = message["result"].get("root", {}).get("nodeId")
                 if nodeid:
                     node = nodeid
+                self.command("DOM.getOuterHTML", nodeId=node)
             if msg_method == "DOM.documentUpdated":
                 self.command("DOM.getDocument")
                 self.nodecmds.append(self.id)
