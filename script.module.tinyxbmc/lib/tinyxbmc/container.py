1ba71f8ecd73e8100f1f4b55a4ed0d467ac586de
boogie <boogiepop@gmx.com>
2023-03-06 23:27:29 +0100

    tinyxbmc: bump version to sync

--- script.module.tinyxbmc/lib/tinyxbmc/container.py
+++ script.module.tinyxbmc/lib/tinyxbmc/container.py
@@ -39,7 +39,7 @@ if REMOTE_DBG:
     pdevpath = "/home/boogie/.p2/pool/plugins/org.python.pydev.core_9.3.0.202203051235/pysrc/"
     sys.path.append(pdevpath)
     import pydevd  # @UnresolvedImport
-    pydevd.settrace(stdoutToServer=True, stderrToServer=True, suspend=False)
+    pydevd.settrace(REMOTE_DBG, stdoutToServer=True, stderrToServer=True, suspend=False)
 
 if PROFILE:
     import pprofile
